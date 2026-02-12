97495fec6002bbe18de5d64df5d61aaa6a2340cd
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-12 20:09:26 +0530

    [saptadipa77]: Added user dto

---
+++ src/main/java/com/gevernova/TaskManagement/dto/UserRequest.java
@@ -0,0 +1,12 @@
+package com.gevernova.TaskManagement.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequest {
+    private String name;
+    private String email;
+}
+

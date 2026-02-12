9b650ec45740be1766fa929d52d77d8def2189db
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-12 19:50:42 +0530

    [saptadipa77]: Added task model

---
+++ src/main/java/com/gevernova/TaskManagement/model/Task.java
@@ -0,0 +1,34 @@
+package com.gevernova.TaskManagement.model;
+import com.gevernova.TaskManagement.model.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Priority priority;
+}
+
